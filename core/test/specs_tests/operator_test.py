633b97327c111d16d54dd38281c7ea6e48c134fa
Anka Laszlo (XC-AS/EAE3-ADA) <Laszlo.Anka@de.bosch.com>
2026-05-07 11:17:17 +0200

    Sanitized and extended replica tests

--- core/test/specs_tests/operator_test.py
+++ core/test/specs_tests/operator_test.py
@@ -749,32 +749,12 @@ class OperatorInstanceTest(unittest.TestCase):
                 or ("operator_b" == nested_instance.name)
             )
 
-    def test_new_replication_all(self):
+    def test_replica_name_retrieval(self):
         pipeline = TestPipelineWithOperator("pipeline")
 
         self.assertEqual("pipeline", pipeline.get_full_name())
         self.assertEqual("pipeline.operator_a", pipeline.operator_a.get_full_name())
         self.assertEqual("pipeline.operator_a_0", pipeline.operator_a_0.get_full_name())
-
-        self.assertIsNot(pipeline.operator_a, pipeline.operator_a_0)
-        self.assertIsNot(pipeline.operator_a_0, pipeline.operator_a)
-        self.assertNotEqual(pipeline.operator_a, pipeline.operator_a_0)
-        self.assertNotEqual(pipeline.operator_a_0, pipeline.operator_a)
-        self.assertNotEqual(pipeline.operator_a_0, pipeline.operator_a_1)
-        self.assertEqual(pipeline.operator_a_0, pipeline.operator_a_0)
-        self.assertIs(pipeline.operator_a_0, pipeline.operator_a_0)
-        self.assertEqual(
-            pipeline.operator_a_0.input_port,
-            pipeline.operator_a_0.input_port,
-        )
-        self.assertNotEqual(hash(pipeline.operator_a), hash(pipeline.operator_a_0))
-        self.assertNotEqual(hash(pipeline.operator_a_0), hash(pipeline.operator_a))
-
-        self.assertTrue(pipeline.operator_a != pipeline.operator_a_0)
-        self.assertTrue(pipeline.operator_a_0 != pipeline.operator_a)
-        self.assertFalse(pipeline.operator_a == pipeline.operator_a_0)
-        self.assertFalse(pipeline.operator_a_0 == pipeline.operator_a)
-
         self.assertEqual(
             "pipeline.operator_a.input_port",
             pipeline.operator_a.input_port.get_full_name(),
@@ -784,10 +764,90 @@ class OperatorInstanceTest(unittest.TestCase):
             pipeline.operator_a_0.input_port.get_full_name(),
         )
 
-        self.assertIs(pipeline.operator_a.input_port.get_context(), pipeline.operator_a)
-        self.assertIs(
-            pipeline.operator_a_0.input_port.get_context(), pipeline.operator_a_0
+    def test_replica_group_information(self):
+        pipeline = TestPipelineWithOperator("pipeline")
+
+        self.assertEqual("pipeline.operator_a", pipeline.operator_a.get_group_name())
+        self.assertIs(pipeline.operator_a, pipeline.operator_a.get_group_principal())
+        self.assertTrue(pipeline.operator_a.is_principal())
+        self.assertEqual(0, pipeline.operator_a.get_group_index())
+
+        self.assertEqual(
+            "pipeline.operator_a.input_port",
+            pipeline.operator_a.input_port.get_group_name(),
         )
+        self.assertIs(
+            pipeline.operator_a.input_port,
+            pipeline.operator_a.input_port.get_group_principal(),
+        )
+        self.assertTrue(pipeline.operator_a.input_port.is_principal())
+        self.assertEqual(0, pipeline.operator_a.input_port.get_group_index())
+
+        group_index = 0
+        for replica in pipeline.operator_a.get_replicas():
+            self.assertEqual("pipeline.operator_a", replica.get_group_name())
+            self.assertIs(pipeline.operator_a, replica.get_group_principal())
+            self.assertFalse(replica.is_principal())
+            self.assertEqual(group_index + 1, replica.get_group_index())
+
+            self.assertEqual(
+                "pipeline.operator_a.input_port", replica.input_port.get_group_name()
+            )
+            self.assertIs(
+                pipeline.operator_a.input_port, replica.input_port.get_group_principal()
+            )
+            self.assertFalse(replica.input_port.is_principal())
+            self.assertEqual(group_index + 1, replica.input_port.get_group_index())
+
+            group_index += 1
+
+    def test_replica_equality_and_identity(self):
+        pipeline = TestPipelineWithOperator("pipeline")
+
+        for replica in pipeline.operator_a.get_replicas():
+            self.assertIsNot(pipeline.operator_a, replica)
+            self.assertNotEqual(hash(pipeline.operator_a), hash(replica))
+
+            # Both direction must be tested as in one case the Instance's
+            # in the other case the ReplicaContext's __eq__ will be called.
+            self.assertNotEqual(pipeline.operator_a, replica)
+            self.assertNotEqual(replica, pipeline.operator_a)
+            self.assertTrue(pipeline.operator_a.is_equivalent_to(replica))
+            self.assertTrue(replica.is_equivalent_to(pipeline.operator_a))
+
+        self.assertNotEqual(pipeline.operator_a_0, pipeline.operator_a_1)
+        self.assertEqual(pipeline.operator_a_0, pipeline.operator_a_0)
+        self.assertIs(pipeline.operator_a_0, pipeline.operator_a_0)
+
+        for replica in pipeline.operator_a.get_replicas():
+            self.assertIsNot(pipeline.operator_a.input_port, replica.input_port)
+            self.assertNotEqual(
+                hash(pipeline.operator_a.input_port), hash(replica.input_port)
+            )
+
+            # Both direction must be tested as in one case the Instance's
+            # in the other case the ReplicaContext's __eq__ will be called.
+            self.assertNotEqual(pipeline.operator_a.input_port, replica.input_port)
+            self.assertNotEqual(replica.input_port, pipeline.operator_a.input_port)
+            self.assertTrue(
+                pipeline.operator_a.input_port.is_equivalent_to(replica.input_port)
+            )
+            self.assertTrue(
+                replica.input_port.is_equivalent_to(pipeline.operator_a.input_port)
+            )
+
+        self.assertNotEqual(
+            pipeline.operator_a_0.input_port, pipeline.operator_a_1.input_port
+        )
+        self.assertEqual(
+            pipeline.operator_a_0.input_port, pipeline.operator_a_0.input_port
+        )
+        self.assertIs(
+            pipeline.operator_a_0.input_port, pipeline.operator_a_0.input_port
+        )
+
+    def test_replica_dto_creation(self):
+        pipeline = TestPipelineWithOperator("pipeline")
 
         operator_a_dto = pipeline.operator_a.get_dto()
         operator_a_0_dto = pipeline.operator_a_0.get_dto()
@@ -795,73 +855,12 @@ class OperatorInstanceTest(unittest.TestCase):
         self.assertEqual("operator_a", operator_a_dto.name)
         self.assertEqual("operator_a_0", operator_a_0_dto.name)
 
-        self.assertEqual("pipeline.operator_a", pipeline.operator_a.get_group_name())
-        self.assertEqual("pipeline.operator_a", pipeline.operator_a_0.get_group_name())
-
-        self.assertIs(pipeline.operator_a.get_group_principal(), pipeline.operator_a)
-        self.assertIs(pipeline.operator_a_0.get_group_principal(), pipeline.operator_a)
-
-        self.assertTrue(pipeline.operator_a.is_principal())
-        self.assertFalse(pipeline.operator_a_0.is_principal())
-
-        self.assertEqual(0, pipeline.operator_a.get_group_index())
-        self.assertEqual(1, pipeline.operator_a_0.get_group_index())
-
-        self.assertEqual(
-            "pipeline.operator_a.input_port",
-            pipeline.operator_a.input_port.get_group_name(),
-        )
-        self.assertEqual(
-            "pipeline.operator_a.input_port",
-            pipeline.operator_a_0.input_port.get_group_name(),
-        )
-
-        self.assertIs(
-            pipeline.operator_a.input_port.get_group_principal(),
-            pipeline.operator_a.input_port,
-        )
-        self.assertIs(
-            pipeline.operator_a_0.input_port.get_group_principal(),
-            pipeline.operator_a.input_port,
-        )
-
-        self.assertTrue(pipeline.operator_a.input_port.is_principal())
-        self.assertFalse(pipeline.operator_a_0.input_port.is_principal())
-
-        self.assertEqual(0, pipeline.operator_a.input_port.get_group_index())
-        self.assertEqual(1, pipeline.operator_a_0.input_port.get_group_index())
-
-        self.assertIsNot(
-            pipeline.operator_a.input_port,
-            pipeline.operator_a_0.input_port,
-        )
-
-        self.assertTrue(
-            pipeline.operator_a.input_port.is_equivalent_to(
-                pipeline.operator_a_0.input_port
-            )
-        )
-
-        self.assertTrue(
-            pipeline.operator_a_0.input_port.is_equivalent_to(
-                pipeline.operator_a_1.input_port
-            )
-        )
-
-        self.assertEqual(
-            pipeline.operator_a_0.input_port,
-            pipeline.operator_a_0.input_port,
-        )
-
-        self.assertNotEqual(
-            hash(pipeline.operator_a.input_port),
-            hash(pipeline.operator_a_0.input_port),
-        )
-
-        self.assertNotEqual(
-            hash(pipeline.operator_a_0.input_port),
-            hash(pipeline.operator_a_1.input_port),
-        )
+    def test_replica_nested_instance_wrapping(self):
+        """
+        It is to be ensured that the Instance object in the nested instance list are
+        wrapped into ReplicaContext, if accessed through the replica context.
+        """
+        pipeline = TestPipelineWithOperator("pipeline")
 
         for plugin in Internals(pipeline.operator_a_0).nested_instances.values():
             print(isinstance(plugin, ReplicaContext))
@@ -875,12 +874,8 @@ class OperatorInstanceTest(unittest.TestCase):
             )
 
 
-# Operator replica + plugin
-# str
-# eq
-# hash
-
-# create from dto / string
-# all the above from the created one
-
-# replica creation / deletion -> param setting
+# channel
+# depends_on
+# get_full_name a ctorban
+# tényleg lemennek-e executálás közben a replicált dolgok
+# docs
